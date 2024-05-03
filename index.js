a3087da300523661cf24fcf0f4e2ecff3ee8e272
Kinleyu Namgay <163089744+kinley1234@users.noreply.github.com>
2024-05-03 10:59:40 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-document.getElementById("calculateButton").addEventListener("click", calculateLove);
+document.addEventListener("DOMContentLoaded", function() {
+    const calculateBtn = document.getElementById('calculateBtn');
+    const resultDiv = document.getElementById('result');
 
-function calculateLove() {
-    var playerName = document.getElementById("playerName").value.trim();
-    var crushName = document.getElementById("crushName").value.trim();
+    calculateBtn.addEventListener('click', function() {
+        const weight = parseFloat(document.getElementById('weight').value);
+        const height = parseFloat(document.getElementById('height').value) / 100; // Convert height to meters
+        const bmi = weight / (height * height);
+        const bmiCategory = getBMICategory(bmi);
+        resultDiv.innerHTML = `<p>Your BMI is: ${bmi.toFixed(2)}</p><p>You are ${bmiCategory}</p>`;
+    });
 
-    if (playerName === "" || crushName === "") {
-        alert("Please enter both names.");
-        return;
-    }
-
-    var loveScore = calculateLoveScore(playerName, crushName);
-    var resultText = getResultText(loveScore);
-
-    document.getElementById("result").textContent = resultText;
-}
-
-function calculateLoveScore(playerName, crushName) {
-    var lowerPlayerName = playerName.toLowerCase();
-    var lowerCrushName = crushName.toLowerCase();
-    var matchingChars = 0;
-
-    for (var i = 0; i < lowerPlayerName.length; i++) {
-        for (var j = 0; j < lowerCrushName.length; j++) {
-            if (lowerPlayerName[i] === lowerCrushName[j]) {
-                matchingChars++;
-                lowerCrushName = lowerCrushName.substring(0, j) + lowerCrushName.substring(j + 1);
-                break;
-            }
+    function getBMICategory(bmi) {
+        if (bmi < 18.5) {
+            return "Underweight,try to gain weight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Normal weight";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Overweight, try to reduse weight";
+        } else {
+            return "Obese";
         }
     }
+});
 
-    return Math.round((matchingChars / (playerName.length + crushName.length)*1.5) * 100);
-}
 
-function getResultText(loveScore) {
-    if (loveScore < 40) {
-        return `Your love score is ${loveScore}%. Maybe it's not meant to be...`;
-    } else if (loveScore >= 40 && loveScore < 70) {
-        return `Your love score is ${loveScore}%. There's potential!`;
-    } else {
-        return `Your love score is ${loveScore}%. Wow, sparks are flying!`;
-    }
-}
+
+
+
